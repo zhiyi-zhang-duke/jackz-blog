5b04135c1d1f07c79bb1178fd93093964dc4cef7
Jack Zhang <zhiyi.zhang.duke@gmail.com>
2023-02-20 22:13:17 -0500

    Test push

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,7 @@ import Link from 'next/link'
 export default function Home() {
   return (
     <div>
-      Hello World. <Link href="/about">About</Link>
+      Hello World. This is my blog <Link href="/about">About</Link>
     </div>
   )
 }
