f07a7ec4d25de9589582ffb3cb07cb420f1b21e3
zhiyi-zhang-duke <68999131+zhiyi-zhang-duke@users.noreply.github.com>
2023-02-21 15:04:45 -0500

    Deploy

--- pages/index.tsx
+++
@@ -1,9 +0,0 @@
-import Link from 'next/link'
-
-export default function Home() {
-  return (
-    <div>
-      Hello Jack's World. This is his blog <Link href="/about">About</Link>
-    </div>
-  )
-}
